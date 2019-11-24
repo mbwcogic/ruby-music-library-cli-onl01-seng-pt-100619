6eb8743e7debf4e5737fded0062cc2fe7d47aea6
Michael Waldo <mbwcogic@yahoo.com>
2019-11-24 17:39:46 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -46,4 +46,9 @@ class MusicLibraryController
       puts "#{i}. #{a.name}"
     end
   end
+   def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
+  end
 end
